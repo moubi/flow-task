f09a038ec71ddbe182fb7d3dd3868a4e4ef57018
Miroslav Nikolov <miroslavnikolov@ymail.com>
2019-09-04 15:27:11 +0200

    Distinguish dragging and inline editing. Inline edit happens when task text is clicked. Dragging start when long press/click is triggered (hold for 1s).

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -8,45 +8,73 @@ export default class Task extends Component {
   constructor(props) {
     super(props);
 
+    this.el = null;
     this.buttonPressTimer = null;
+    this.getRef = this.getRef.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
     this.handleButtonPress = this.handleButtonPress.bind(this);
     this.handleButtonRelease = this.handleButtonRelease.bind(this);
   }
 
-  handleButtonPress() {
-    this.buttonPressTimer = setTimeout(() => console.log('long press activated'), 1500);
+  componentDidMount() {
+    // We need a way to avoid contentEditable warnings from react
+    // One way to do so is to add the attribute after mounting
+    if (this.el) {
+      this.el.setAttribute("contentEditable", true);
+    }
   }
 
-  handleButtonRelease() {
+  handleButtonPress(e) {
+    e.stopPropagation();
+
+    this.buttonPressTimer = setTimeout(() => {
+      const { isDragging, onDragStart } = this.props;
+      if (!isDragging) {
+        onDragStart();
+        // At the point when we drop this element may no longer exist
+        this.el && this.el.blur();
+      }
+    }, 1000);
+  }
+
+  handleButtonRelease(e) {
     clearTimeout(this.buttonPressTimer);
   }
 
+  handleBlur(e) {
+    // Stop event so that it doesn't go to the Column
+    e.stopPropagation();
+    if (this.props.isDragging) {
+      // clicking on contentEditable element focuses it automatically,
+      // so we have to force blur when dragging (no keyboard will appear)
+      this.el.blur();
+    }
+  }
+
+  getRef(el) {
+    this.el = el;
+  }
+
   render() {
-    const { id, text, onDragStart, isDragging = false } = this.props;
+    const { id, children, onDragStart, isDragging = false } = this.props;
 
     return (
       <div
         id={id}
+        ref={this.getRef}
         className={classNames("Task", { "Task--dragging": isDragging })}
         draggable
-        // Enable inline edit
-        onDoubleClick={(e) => {
-          e.stopPropagation();
-          console.log("onDoubleClick");
-        }}
-        onDragStart={() => onDragStart()}
-        onClick={(e) => {
-          console.log("onClick");
-          e.stopPropagation();
-          onDragStart();
-        }}
+        autoFocus
+        onDragStart={onDragStart}
+        onClick={this.handleBlur}
+        // TODO: split touch and desktop
         onTouchStart={this.handleButtonPress}
         onTouchEnd={this.handleButtonRelease}
         onMouseDown={this.handleButtonPress}
         onMouseUp={this.handleButtonRelease}
-        onMouseLeave={this.handleButtonRelease}>
+        onMouseLeave={this.handleButtonRelease}
       >
-        {text}
+        {children}
       </div>
     );
   }
@@ -54,7 +82,7 @@ export default class Task extends Component {
 
 Task.propTypes = {
   id: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  children: PropTypes.node,
   isDragging: PropTypes.bool,
   onDragStart: PropTypes.func.isRequired
-}
+};
