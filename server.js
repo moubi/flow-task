f23a0d28ddea058976400ef746ce439c23dfc73e
Miroslav Nikolov <miroslavnikolov@ymail.com>
2019-09-20 15:24:58 +0200

    Add simple db.json file and a get call to the server.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const fs = require("fs");
 // const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/kanban-board", function(req, res) {
-  return res.send("success load");
+  const rawdata = fs.readFileSync("db.json");
+  const data = JSON.parse(rawdata);
+
+  return res.send(data);
 });
 
 app.post("/kanban-board", function(req, res) {
--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import React, { Component } from "react";
 import Board from "./components/Board/Board";
 
 class App extends Component {
+  componentDidMount() {
+    fetch("/kanban-board")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+  }
+
   render() {
     return <Board />;
   }
