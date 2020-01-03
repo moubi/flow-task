220ea9d8855ff6d61fa9076ea290aa0e9b5309af
Miroslav Nikolov <miroslavnikolov@ymail.com>
2020-01-03 11:02:06 +0100

    Introduce redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import ui from "./ui";
 import columns from "./columns";
 import tasks from "./tasks";
 
-const reducers = combineReducers({ ...ui, ...columns, ...tasks });
+const middlewares = [thunk];
+const rootReducer = combineReducers({ ...ui, ...columns, ...tasks });
 
-export default createStore(reducers);
+export default createStore(rootReducer, applyMiddleware(...middlewares));
