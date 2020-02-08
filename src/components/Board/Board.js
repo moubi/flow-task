d8e6d24d778a6b6ca5083aef6f6317ed83d69cc3
Miroslav Nikolov <miroslavnikolov@ymail.com>
2020-02-08 23:29:47 +0100

    Working solution to make both swiping and dragging working good together.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -11,9 +11,13 @@ import Swipeable from "../Swipeable/Swipeable";
 
 import "./Board.scss";
 
+let VIEWPORT_WIDTH = 0;
+
 const stopImmediatePropagation = e => {
   e.stopImmediatePropagation();
 };
+
+const getColumnIndexAtPosition = scrollX => Math.floor(scrollX/VIEWPORT_WIDTH)
 
 export class Board extends Component {
   constructor(props) {
@@ -21,15 +25,21 @@ export class Board extends Component {
 
     this.el = null;
     this.state = {
-      columnIndexInView: 0
+      swipeStyle: { left: 0 }
     };
 
     this.handleDragEnd = this.handleDragEnd.bind(this);
     this.handleSwipeLeft = this.handleSwipeLeft.bind(this);
     this.handleSwipeRight = this.handleSwipeRight.bind(this);
+    this.handleOnBeforeDragCapture = this.handleOnBeforeDragCapture.bind(this);
     // This is needed in order to disable dragging with keyboard
     // It doesn't work if placed in componentWillMount
     window.addEventListener("keydown", stopImmediatePropagation, true);
+  }
+
+  componentDidMount() {
+    const columnsLength = Object.keys(this.props.columns).length;
+    VIEWPORT_WIDTH = this.el.offsetWidth/columnsLength;
   }
 
   componentWillUnmount() {
@@ -37,37 +47,68 @@ export class Board extends Component {
   }
 
   handleDragEnd({ source, destination }) {
-    if (!destination) {
-      return;
+    if (destination) {
+      const { columns, moveTask } = this.props;
+      const draggedTaskId = columns[source.droppableId].tasks[source.index];
+      const destinationColumnId = columns[destination.droppableId].id;
+
+      moveTask(draggedTaskId, destinationColumnId, destination.index);
     }
 
-    const { columns, moveTask } = this.props;
-    const draggedTaskId = columns[source.droppableId].tasks[source.index];
-    const destinationColumnId = columns[destination.droppableId].id;
-
-    moveTask(draggedTaskId, destinationColumnId, destination.index);
+    this.setState({
+      swipeStyle: {
+        position: "fixed",
+        left: `${-1 * window.scrollX}px`
+      }
+    });
   }
 
   handleSwipeLeft() {
-    const { columnIndexInView } = this.state;
+    const { swipeStyle } = this.state;
+    const columnIndexInView = getColumnIndexAtPosition(Math.abs(parseInt(swipeStyle.left)));
+
     if (columnIndexInView < 2) {
-      this.setState({ columnIndexInView: columnIndexInView + 1 });
+      this.setState({
+        swipeStyle: {
+          ...swipeStyle,
+          left: (columnIndexInView + 1) * -VIEWPORT_WIDTH + "px",
+          transition: "left 0.2s ease-in"
+        }
+      });
     }
   }
 
   handleSwipeRight() {
-    const { columnIndexInView } = this.state;
+    const { swipeStyle } = this.state;
+    const columnIndexInView = getColumnIndexAtPosition(Math.abs(parseInt(swipeStyle.left)));
+
     if (columnIndexInView > 0) {
-      this.setState({ columnIndexInView: columnIndexInView - 1 });
+      this.setState({
+        swipeStyle: {
+          ...swipeStyle,
+          left: (columnIndexInView - 1) * -VIEWPORT_WIDTH + "px",
+          transition: "left 0.2s ease-in"
+        }
+      });
     }
+  }
+
+  handleOnBeforeDragCapture() {
+    const { swipeStyle } = this.state;
+    this.setState({
+      swipeStyle: {
+        ...swipeStyle,
+        position: "initial"
+      }
+    });
+    // This is needed if we have swiped to other columns previously
+    // It prevents jumping to the first column when drag starts
+    window.scroll(-1 * parseInt(swipeStyle.left), 0);
   }
 
   render() {
     const { columns, tasks } = this.props;
-    const { columnIndexInView } = this.state;
-    const columnsArray = Object.values(columns);
-    // this.el.offsetWidth will return the width of the Board 960px (iPhone SE)
-    const scrollWidth = columnIndexInView * (this.el ? -this.el.offsetWidth/columnsArray.length : 0);
+    const { swipeStyle } = this.state;
     const hasTasks = Object.keys(tasks).length > 0;
 
     return (
@@ -83,10 +124,13 @@ export class Board extends Component {
               this.el = el;
               innerRef(el);
             }}
-            style={{ left: `${scrollWidth}px`}}
+            style={{ ...swipeStyle }}
           >
-            <DragDropContext onDragEnd={this.handleDragEnd} >
-              {columnsArray.map(column => (
+            <DragDropContext
+              onDragEnd={this.handleDragEnd}
+              onBeforeCapture={this.handleOnBeforeDragCapture}
+            >
+              {Object.values(columns).map(column => (
                 <Droppable key={column.id} droppableId={column.id} type="COLUMN">
                   {(provided, snapshot) => (
                     <Column
@@ -109,7 +153,7 @@ export class Board extends Component {
                               >
                                 {(provided, snapshot) => (
                                   // TODO: Think about removing this wrapper
-                                  // at some point and use <Tack /> directly
+                                  // at some point and use <Task /> directly
                                   <div
                                     ref={provided.innerRef}
                                     {...provided.draggableProps}
