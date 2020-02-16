5cb4b105396ed611ea299334297f9ffd8e7d79f9
Miroslav Nikolov <miroslavnikolov@ymail.com>
2020-02-16 21:59:39 +0100

    wip: Swipeable from class to function with hooks.

--- src/components/Swipeable/Swipeable.js
+++ src/components/Swipeable/Swipeable.js
@@ -1,51 +1,31 @@
-import { PureComponent } from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 
-// TODO: Rewrite using Hooks
-export default class Swipeable extends PureComponent {
-  constructor(props) {
-    super(props);
+// React Hooks version
+export default function Swipeable(props) {
+  let el = null;
+  let touchStartedTime = null;
+  let clientX = null;
+  let clientY = null;
+  let clientXDiff = null;
+  let clientYDiff = null;
 
-    this.el = null;
-    this.touchStartedTime = null;
-    this.clientX = null;
-    this.clientY = null;
-    this.clientXDiff = null;
-    this.clientYDiff = null;
-
-    this.handleTouchStart = this.handleTouchStart.bind(this);
-    this.handleTouchMove = this.handleTouchMove.bind(this);
-    this.handleTouchEnd = this.handleTouchEnd.bind(this);
+  function handleTouchStart(e) {
+    touchStartedTime = Date.now();
+    clientX = e.touches[0].clientX;
+    clientY = e.touches[0].clientY;
+    clientXDiff = 0;
+    clientYDiff = 0;
   }
 
-  componentDidMount() {
-    this.el.addEventListener("touchstart", this.handleTouchStart);
-    this.el.addEventListener("touchmove", this.handleTouchMove);
-    this.el.addEventListener("touchend", this.handleTouchEnd);
+  function handleTouchMove(e) {
+    if (!clientX || !clientY) return;
+
+    clientXDiff = clientX - e.touches[0].clientX;
+    clientYDiff = clientY - e.touches[0].clientY;
   }
 
-  componentWillUnmount() {
-    this.el.removeEventListener("touchstart", this.handleTouchStart);
-    this.el.removeEventListener("touchmove", this.handleTouchMove);
-    this.el.removeEventListener("touchend", this.handleTouchEnd);
-  }
-
-  handleTouchStart(e) {
-    this.touchStartedTime = Date.now();
-    this.clientX = e.touches[0].clientX;
-    this.clientY = e.touches[0].clientY;
-    this.clientXDiff = 0;
-    this.clientYDiff = 0;
-  }
-
-  handleTouchMove(e) {
-    if (!this.clientX || !this.clientY) return;
-
-    this.clientXDiff = this.clientX - e.touches[0].clientX;
-    this.clientYDiff = this.clientY - e.touches[0].clientY;
-  }
-
-  handleTouchEnd(e) {
+  function handleTouchEnd(e) {
     const {
       minDistance = 20,
       maxDistance = Infinity,
@@ -54,17 +34,17 @@ export default class Swipeable extends PureComponent {
       onSwipeRight,
       onSwipeUp,
       onSwipeDown
-    } = this.props;
-    const timeDiff = Date.now() - this.touchStartedTime;
-    const clientXDiffAbs = Math.abs(this.clientXDiff);
-    const clientYDiffAbs = Math.abs(this.clientYDiff);
+    } = props;
+    const timeDiff = Date.now() - touchStartedTime;
+    const clientXDiffAbs = Math.abs(clientXDiff);
+    const clientYDiffAbs = Math.abs(clientYDiff);
 
     // Horizontal swipe
     if (clientXDiffAbs > clientYDiffAbs) {
       if (clientXDiffAbs >= minDistance && clientXDiffAbs <= maxDistance && timeDiff <= timeout) {
         // Prevent other swipeables
         e.stopPropagation();
-        if (this.clientXDiff > 0) {
+        if (clientXDiff > 0) {
           onSwipeLeft && onSwipeLeft();
         } else {
           onSwipeLeft && onSwipeRight();
@@ -75,7 +55,7 @@ export default class Swipeable extends PureComponent {
       if (clientYDiffAbs >= minDistance && clientXDiffAbs <= maxDistance && timeDiff <= timeout) {
         // Prevent other swipeables
         e.stopPropagation();
-        if (this.clientYDiff > 0) {
+        if (clientYDiff > 0) {
           onSwipeUp && onSwipeUp();
         } else {
           onSwipeDown && onSwipeDown();
@@ -83,19 +63,34 @@ export default class Swipeable extends PureComponent {
       }
     }
 
-    this.clientX = null;
-    this.clientY = null;
-    this.touchStartedTime = null;
+    clientX = null;
+    clientY = null;
+    touchStartedTime = null;
   }
 
-  render() {
-    const { children } = this.props;
+  // handleTouchStart = handleTouchStart.bind(that);
+  // handleTouchMove = handleTouchMove.bind(that);
+  // handleTouchEnd = handleTouchEnd.bind(that);
 
-    return children(el => {
-      this.el = el;
-    });
-  }
-};
+  useEffect(() => {
+    console.log(el)
+    // componentDidMount
+    el.addEventListener("touchstart", handleTouchStart);
+    el.addEventListener("touchmove", handleTouchMove);
+    el.addEventListener("touchend", handleTouchEnd);
+
+    // componentWillUnmount
+    return () => {
+      el.removeEventListener("touchstart", handleTouchStart);
+      el.removeEventListener("touchmove", handleTouchMove);
+      el.removeEventListener("touchend", handleTouchEnd);
+    };
+  }, [el]);
+
+  return props.children(DOMNode => {
+    el = DOMNode;
+  });
+}
 
 Swipeable.propTypes = {
   children: PropTypes.func.isRequired,
@@ -107,3 +102,110 @@ Swipeable.propTypes = {
   onSwipeUp: PropTypes.func,
   onSwipeDown: PropTypes.func
 };
+
+// TODO: Rewrite using Hooks
+// export default class Swipeable extends PureComponent {
+//   constructor(props) {
+//     super(props);
+
+//     this.el = null;
+//     this.touchStartedTime = null;
+//     this.clientX = null;
+//     this.clientY = null;
+//     this.clientXDiff = null;
+//     this.clientYDiff = null;
+
+//     this.handleTouchStart = this.handleTouchStart.bind(this);
+//     this.handleTouchMove = this.handleTouchMove.bind(this);
+//     this.handleTouchEnd = this.handleTouchEnd.bind(this);
+//   }
+
+//   componentDidMount() {
+//     this.el.addEventListener("touchstart", this.handleTouchStart);
+//     this.el.addEventListener("touchmove", this.handleTouchMove);
+//     this.el.addEventListener("touchend", this.handleTouchEnd);
+//   }
+
+//   componentWillUnmount() {
+//     this.el.removeEventListener("touchstart", this.handleTouchStart);
+//     this.el.removeEventListener("touchmove", this.handleTouchMove);
+//     this.el.removeEventListener("touchend", this.handleTouchEnd);
+//   }
+
+//   handleTouchStart(e) {
+//     this.touchStartedTime = Date.now();
+//     this.clientX = e.touches[0].clientX;
+//     this.clientY = e.touches[0].clientY;
+//     this.clientXDiff = 0;
+//     this.clientYDiff = 0;
+//   }
+
+//   handleTouchMove(e) {
+//     if (!this.clientX || !this.clientY) return;
+
+//     this.clientXDiff = this.clientX - e.touches[0].clientX;
+//     this.clientYDiff = this.clientY - e.touches[0].clientY;
+//   }
+
+//   handleTouchEnd(e) {
+//     const {
+//       minDistance = 20,
+//       maxDistance = Infinity,
+//       timeout = 500,
+//       onSwipeLeft,
+//       onSwipeRight,
+//       onSwipeUp,
+//       onSwipeDown
+//     } = this.props;
+//     const timeDiff = Date.now() - this.touchStartedTime;
+//     const clientXDiffAbs = Math.abs(this.clientXDiff);
+//     const clientYDiffAbs = Math.abs(this.clientYDiff);
+
+//     // Horizontal swipe
+//     if (clientXDiffAbs > clientYDiffAbs) {
+//       if (clientXDiffAbs >= minDistance && clientXDiffAbs <= maxDistance && timeDiff <= timeout) {
+//         // Prevent other swipeables
+//         e.stopPropagation();
+//         if (this.clientXDiff > 0) {
+//           onSwipeLeft && onSwipeLeft();
+//         } else {
+//           onSwipeLeft && onSwipeRight();
+//         }
+//       }
+//     // Vertical swipe
+//     } else {
+//       if (clientYDiffAbs >= minDistance && clientXDiffAbs <= maxDistance && timeDiff <= timeout) {
+//         // Prevent other swipeables
+//         e.stopPropagation();
+//         if (this.clientYDiff > 0) {
+//           onSwipeUp && onSwipeUp();
+//         } else {
+//           onSwipeDown && onSwipeDown();
+//         }
+//       }
+//     }
+
+//     this.clientX = null;
+//     this.clientY = null;
+//     this.touchStartedTime = null;
+//   }
+
+//   render() {
+//     const { children } = this.props;
+
+//     return children(el => {
+//       this.el = el;
+//     });
+//   }
+// };
+
+// Swipeable.propTypes = {
+//   children: PropTypes.func.isRequired,
+//   minDistance: PropTypes.number,
+//   maxDistance: PropTypes.number,
+//   timeout: PropTypes.number,
+//   onSwipeLeft: PropTypes.func,
+//   onSwipeRight: PropTypes.func,
+//   onSwipeUp: PropTypes.func,
+//   onSwipeDown: PropTypes.func
+// };
