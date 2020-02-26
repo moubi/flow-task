b23cb2db7cbd9a598d67bfd40a0826eb5be9c9be
Miroslav Nikolov <miroslavnikolov@ymail.com>
2020-02-26 12:57:07 +0100

    Refactor Api.js

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,64 +1,54 @@
+const headers = { "Content-Type": "application/json" };
+
 export default {
   loadColumns: () => {
     return fetch(`api/columns/`, {
       method: "GET",
-      headers: { "Content-Type": "application/json" }
+      headers
     })
       .then(response => response.json())
-      .then(data => {
-        return Promise.resolve(data);
-      });
+      .then(data => Promise.resolve(data));
   },
   loadTasks: () => {
     return fetch(`api/tasks/`, {
       method: "GET",
-      headers: { "Content-Type": "application/json" }
+      headers
     })
       .then(response => response.json())
-      .then(data => {
-        return Promise.resolve(data);
-      });
+      .then(data => Promise.resolve(data));
   },
   deleteTask: id => {
     return fetch(`api/tasks/?id=${id}`, {
       method: "DELETE",
-      headers: { "Content-Type": "application/json" }
+      headers
     })
       .then(response => response.json())
-      .then(data => {
-        return Promise.resolve(data);
-      });
+      .then(data => Promise.resolve(data));
   },
   updateTask: (id, data) => {
     return fetch(`api/tasks/?id=${id}`, {
       method: "POST",
       body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
+      headers
     })
       .then(response => response.json())
-      .then(data => {
-        return Promise.resolve(data);
-      });
+      .then(data => Promise.resolve(data));
   },
   createTask: id => {
     return fetch(`api/tasks/?id=${id}`, {
       method: "PUT",
-      headers: { "Content-Type": "application/json" }
+      headers
     })
       .then(response => response.json())
-      .then(data => {
-        return Promise.resolve(data);
-      });
+      .then(data => Promise.resolve(data));
   },
   updateColumn: (id, data) => {
     return fetch(`api/columns/?id=${id}`, {
       method: "POST",
       body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
+      headers
     })
       .then(response => response.json())
-      .then(data => {
-        return Promise.resolve(data);
-      });
+      .then(data => Promise.resolve(data));
   }
 };
