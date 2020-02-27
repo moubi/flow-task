855cdcec9b9ff19fd738f5c77fb0cd14802e802c
Miroslav Nikolov <miroslavnikolov@ymail.com>
2020-02-27 17:45:42 +0100

    Add PropUpdater wrapper in unexpected-react utils

--- src/testUtils/unexpected-react.js
+++ src/testUtils/unexpected-react.js
@@ -5,6 +5,7 @@ import ReactDom from "react-dom";
 import React, { Component } from "react";
 import unexpectedSinon from "unexpected-sinon";
 import PropTypes from "prop-types";
+import { simulate } from "react-dom-testing";
 
 import FakeReduxProvider from "./FakeReduxProvider";
 
@@ -28,10 +29,17 @@ export function getInstance(reactElement, tagName = "div") {
   const div = document.createElement(tagName);
   const element = ReactDom.render(reactElement, div);
 
-  return {
+  const result = {
     instance: element,
     subject: div.firstChild
   };
+
+  if (reactElement.type === PropUpdater) {
+    result.applyPropsUpdate = () =>
+      simulate(result.subject.firstChild, { type: "click" });
+  }
+
+  return result;
 }
 
 export function withStore(Component, initialState, mockApi, reducer) {
@@ -45,6 +53,37 @@ export function withStore(Component, initialState, mockApi, reducer) {
     </FakeReduxProvider>
   );
 }
+
+export class PropUpdater extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isClicked: false
+    };
+  }
+
+  render() {
+    const { children, propsUpdate } = this.props;
+    const { isClicked } = this.state;
+
+    let child;
+    if (isClicked) {
+      child = React.cloneElement(children, propsUpdate);
+    } else {
+      child = children;
+    }
+
+    return (
+      <div onClick={() => this.setState({ isClicked: true })}>{child}</div>
+    );
+  }
+}
+
+PropUpdater.propTypes = {
+  children: PropTypes.element.isRequired,
+  propsUpdate: PropTypes.object.isRequired
+};
 
 export { Ignore, simulate } from "react-dom-testing";
 export default expect;
